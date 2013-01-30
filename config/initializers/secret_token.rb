07e8f56570bf6fe8d3b86843d4202b5a22756195
david@improvz.com <david@improvz.com>
2013-01-30 16:35:59 -0500

    ruby initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comfesite::Application.config.secret_token = '0e6a9bb29257c52dfbdc0fc81135fcf52554e15b4439c17153c759056d215231f2b5b147f849c86ec5a8c05f8ac5cab7f806247ec18ae21756546fefe7ee2e58'
