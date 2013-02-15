260c583d83e103fe0b1d80ad56d553efb59d063f
david@improvz.com <david@improvz.com>
2013-02-15 08:44:50 -0500

    helper functon for body id

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,38 @@ module ApplicationHelper
 	    content_for(:head) { stylesheet_link_tag(*files) }
 	end
 	
+	def setBodyId(title)
+		@bodyID
+		case 
+		when title == "Comfe Designs"
+			@bodyID = "index"
+
+		when title == "Comfe Arms" 
+			@bodyID = "arms"
+
+		when title == "Comfe Hands"
+			@bodyID = "hands"
+
+		when title == "About Comfe Designs"
+			@bodyID = "about"
+
+		when title == "Comfe Designs News"
+			@bodyID = "news"
+
+		when title == "Comfe Designs Sitemap"
+			@bodyID = "sitemap"
+
+		when title == "Contact Comfe Designs"
+			@bodyID = "contact"
+
+		when title == "Comfe Designs Legal"
+			@bodyID = "legal"
+		
+		else
+			@bodyID = "error"
+		end
+
+		return @bodyID
+	end
+
 end
